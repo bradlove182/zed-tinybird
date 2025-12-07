a310e158911fe2cd587d2cdb504c43b750a042f3
Bradley Love <brad@bradlove.co.za>
2025-12-07 12:48:55 +0200

    partial syntax highlighting

--- languages/tinybird-datasource/highlights.scm
+++ languages/tinybird-datasource/highlights.scm
@@ -1,18 +1,5 @@
-; Inherit SQL highlighting
-; (this extends the built-in SQL grammar highlights)
+; Comments
+(comment) @comment
 
-; Tinybird-specific keywords as identifiers
-((identifier) @keyword
-  (#match? @keyword "^(SCHEMA|ENGINE|ENGINE_SORTING_KEY|ENGINE_PARTITION_KEY|ENGINE_TTL|ENGINE_VER|ENGINE_SIGN|ENGINE_VERSION|ENGINE_SETTINGS|FORWARD_QUERY|BACKFILL|TOKEN|SHARED_WITH)$"))
-
-; Engine types
-((identifier) @type.builtin
-  (#match? @type.builtin "^(MergeTree|ReplacingMergeTree|CollapsingMergeTree|VersionedCollapsingMergeTree)$"))
-
-; Permissions
-((identifier) @constant.builtin
-  (#match? @constant.builtin "^(READ|APPEND|skip)$"))
-
-; ClickHouse/SQL functions specific to Tinybird
-((identifier) @function.builtin
-  (#match? @function.builtin "^(toYYYYMM|toStartOfDay|toIntervalDay|LowCardinality|DateTime|String|Int8|Int16|Int32|Int64|UInt8|UInt16|UInt32|UInt64|Float32|Float64)$"))
+; All Tinybird keywords (instructions, types, SQL keywords, functions)
+(instruction_keyword) @keyword
--- languages/tinybird-pipe/highlights.scm
+++ languages/tinybird-pipe/highlights.scm
@@ -1,22 +1,5 @@
-; Inherit SQL highlighting
-; (this extends the built-in SQL grammar highlights)
+; Comments
+(comment) @comment
 
-; Tinybird pipe-specific keywords as identifiers
-((identifier) @keyword
-  (#match? @keyword "^(DESCRIPTION|TAGS|NODE|SQL|TYPE|DATASOURCE|TARGET_DATASOURCE|TOKEN|COPY_SCHEDULE|COPY_MODE|DEPLOYMENT_METHOD)$"))
-
-; Pipe types
-((identifier) @type.builtin
-  (#match? @type.builtin "^(ENDPOINT|MATERIALIZED|COPY|SINK)$"))
-
-; Permissions and modes
-((identifier) @constant.builtin
-  (#match? @constant.builtin "^(READ|APPEND|append|replace|alter)$"))
-
-; Template functions (Tinybird templating syntax: {{ String(...) }})
-((identifier) @function.special
-  (#match? @function.special "^(String|Int32|Int64|Float32|Float64|Date|DateTime|UInt32|UInt64)$"))
-
-; ClickHouse/SQL functions specific to Tinybird
-((identifier) @function.builtin
-  (#match? @function.builtin "^(toYYYYMM|toStartOfDay|toStartOfHour|toIntervalDay|toIntervalHour|now|LowCardinality|sum|count|avg|min|max)$"))
+; All Tinybird keywords (instructions, types, SQL keywords, functions)
+(instruction_keyword) @keyword
